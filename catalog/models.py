552dbc4fd93f0248651919480633a583f9e733a4
KonstantinMikheev <mikhey17@gmail.com>
2024-08-16 14:24:09 +0300

    add homework 20.1 Работа с ORM в Django

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {"blank": True, "null": True}  # Делает поле необязательным к заполнению
+
+
+class Category(models.Model):
+    title = models.CharField(
+        max_length=100,
+        verbose_name="Наименование",
+        help_text="Введите наименование категории",
+    )
+    description = models.TextField(
+        verbose_name="Описание", help_text="Введите описание категории", **NULLABLE
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ("title",)
+
+
+class Product(models.Model):
+    title = models.CharField(
+        max_length=100,
+        verbose_name="Наименование",
+        help_text="Введите наименование продукта",
+    )
+    description = models.TextField(
+        verbose_name="Описание", help_text="Введите описание продукта", **NULLABLE
+    )
+    preview = models.ImageField(
+        upload_to="products/image", verbose_name="Изображение", **NULLABLE
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, **NULLABLE, related_name="products"
+    )
+    price = models.IntegerField(verbose_name="Цена за покупку")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = (
+            "title",
+            "category",
+            "price",
+        )
--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hw_django',
+        'USER': 'postgres',
+        'PASSWORD': 951225,
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
@@ -120,6 +124,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (BASE_DIR / 'static',)
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
